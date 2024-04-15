75ea0173b6e51195651f6cf035b3c6efe0397848
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-04-15 12:22:06 +0100

    initial desktop css added

--- portfolio-website-react/src/Components/ContactForm.jsx
+++ portfolio-website-react/src/Components/ContactForm.jsx
@@ -4,31 +4,33 @@ import sendIcon from "../assets/form-img/send-fill.svg";
 
 const ContactForm = () => {
   return (
-    <div className="contact-form-container">
-      <h2>Contact</h2>
-      <form id="contact-form" name="contact-form" method="POST">
-        <input type="hidden" name="form-name" value="contact-form" />
-        <input required type="text" id="name-input" placeholder="Full Name" />
-        <input
-          required
-          type="email"
-          id="email-input"
-          placeholder="Email Address"
-        />
-        <textarea
-          name="message-input"
-          id="message-input"
-          cols="30"
-          rows="10"
-          placeholder="Write Your Message..."
-          required
-          maxLength={250}
-        ></textarea>
-        <button type="submit" id="submit-form-button">
-          Send Message
-          <img src={sendIcon}></img>
-        </button>
-      </form>
+    <div className="contact-section">
+      <div className="contact-form-container">
+        <h2>Contact</h2>
+        <form id="contact-form" name="contact-form" method="POST">
+          <input type="hidden" name="form-name" value="contact-form" />
+          <input required type="text" id="name-input" placeholder="Full Name" />
+          <input
+            required
+            type="email"
+            id="email-input"
+            placeholder="Email Address"
+          />
+          <textarea
+            name="message-input"
+            id="message-input"
+            cols="30"
+            rows="10"
+            placeholder="Write Your Message..."
+            required
+            maxLength={250}
+          ></textarea>
+          <button type="submit" id="submit-form-button">
+            Send Message
+            <img src={sendIcon}></img>
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
